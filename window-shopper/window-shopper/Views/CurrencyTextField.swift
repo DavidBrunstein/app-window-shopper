b4ab699ae816f3816f35973ac90a8bde777470bc
Davo Brunstein <davidbrunstein@gmail.com>
2017-12-25 18:25:13 -0600

    dollar sign on the text fields

--- window-shopper/window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -20,10 +20,28 @@ class CurrencyTextField: UITextField {
         customizeTextFieldView()
     }
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.7203935551, green: 0.7203935551, blue: 0.7203935551, alpha: 0.8)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        
+        // Grabbing the currency symbol based on the locale
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        
+        addSubview(currencyLabel)
+    }
     func customizeTextFieldView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let placeNotNil = placeholder {
             let place = NSAttributedString(string: placeNotNil, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
